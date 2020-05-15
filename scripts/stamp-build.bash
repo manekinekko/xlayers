757f4e379333452ccc20d09be433580627e2d31e
Wassim Chegham <1699357+manekinekko@users.noreply.github.com>
2020-05-15 10:41:37 +0200

    ci: update stamp script

--- scripts/stamp-build.bash
+++ scripts/stamp-build.bash
@@ -3,21 +3,22 @@
 set -o errexit
 set -o pipefail
 
-if [[ -n "$BUILD_ID" ]]; then
-    echo "Cloud Build environment detected!"
-    version="${BUILD_ID}"
+if [ -z "$GITHUB_SHA" ]
+then
+   b=`git rev-parse --abbrev-ref HEAD`
+   v=`git rev-parse --short HEAD`
+   version="$b+sha.$v"
 else
-    b=`git rev-parse --abbrev-ref HEAD`
-    v=`git rev-parse --short HEAD`
-    version="$b+sha.$v"
+   v=`echo $GITHUB_SHA | cut -c1-8`
+   version="build: $v"
 fi
 
 ## replease _BUILD_HASH_ with the current build number
-perl -i -pe "s/_BUILD_HASH_/$version/g" dist/html/*.js
+perl -i -pe "s/_BUILD_HASH_/$version/g" dist/ui/index.html
 
 status=$?
 if [ $status -eq 0 ];then
-   echo "Build was stamped: $version"
+   echo ">> Build was stamped: $version"
 else
-   echo "Could not stamp this build!"
+   echo ">> Could not stamp this build!"
 fi
